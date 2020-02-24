e1a548cfe21ffefc524caad8aaea5641ac7988b6
Emile-Dadou-EPSI <57677644+emile-dadou-epsi@users.noreply.github.com>
2020-02-24 16:20:38 +0100

    add image from rover on mars display and error message when image not load (for example if the type of the file isn't an image)

--- venv/nasa/APOD.py
+++ venv/nasa/APOD.py
@@ -1,22 +1,45 @@
 import urllib3
 import shutil
-
+import json
+import requests
 global path
 
 
 def nasa_image():
     ## Url for the nasa web service to get the today image from space
-    url = "https://apod.nasa.gov/apod/image/2002/Eta-HST-ESO-New-LL.jpg"
+    urldemo = "https://api.nasa.gov/planetary/apod?api_key=O6lcyF8XnWi0w3nWQseJcvjeECQUmIRhRxyrya2R"
+    urldemo2 = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=O6lcyF8XnWi0w3nWQseJcvjeECQUmIRhRxyrya2R"
+
+    r = requests.get(urldemo)
+    r2 = requests.get(urldemo2)
+
+    json_obj2 = r2.json()
+    print(json_obj2)
+    url_img_mars = json_obj2["photos"][0]["img_src"]
+    print(url_img_mars)
+
+    json_obj = r.json()
+    url_img_type = json_obj["media_type"]
+    url_image = json_obj["url"]
+    url = url_image
 
     c = urllib3.PoolManager()
 
-    # get the image from the url and save it into the test.png file
-    with c.request('GET', url, preload_content=False) as resp, open("test.png", 'wb') as out_file:
-        shutil.copyfileobj(resp, out_file)
+    if (url_img_type == 'photo'):
+
+        # get the image from the url and save it into the test.png file
+        with c.request('GET', url, preload_content=False) as resp, open("test.png", 'wb') as out_file:
+            shutil.copyfileobj(resp, out_file)
 
 
 
-    resp.release_conn()
+        resp.release_conn()
+
+    else :
+        with c.request('GET', url_img_mars, preload_content=False) as resp, open("test2.png", 'wb') as out_file:
+            shutil.copyfileobj(resp, out_file)
+
+        resp.release_conn()
 
 def get_img_path():
     path = "test.png"
--- venv/nasa/window_app.py
+++ venv/nasa/window_app.py
@@ -1,7 +1,10 @@
 from tkinter import *
+from tkinter import messagebox
 import APOD
 from PIL import ImageTk, Image
 import os
+
+list_path = ['']
 
 # Initialization of the frame
 class Window(Frame):
@@ -12,30 +15,64 @@ class Window(Frame):
 
 
 # Close window
-def close_window():
-    os.remove("test.png")
+def close_window(self, path_list):
+    for x in path_list:
+        if (x != "" and x != None):
+            os.remove(x)
+
     root.destroy()
 
 
 # function to display image in the frame
 def displayImages(self, path):
-    load = Image.open(path)
-    render = ImageTk.PhotoImage(load)
-    label = Label(self, image = render, bg = "black")
-    label.image = render
-    label.pack()
+    if (path == ""):
+        messagebox.showinfo("The image " + path + " doesn't exist", "error")
+        return
+    else:
+        load = Image.open(path)
+        render = ImageTk.PhotoImage(load)
+        label = Label(self, image = render, bg = "black")
+        label.image = render
+        label.pack()
 
 
 
 root = Tk()
 
 app = Window(root)
-root.wm_title("Tkinter window")
+root.wm_title("NasaPy")
 root.geometry("800x600")
 
-button = Button(root,text = "Display image", command= lambda : displayImages(root, "test.png") )
-button2 = Button(root,text = "Exit", command=close_window )
+path_img_1 = ""
+path_img_2 = ""
+
+# Verification that the images exists
+if (os.path.exists("test.png")):
+    # Open image and process it
+    image = Image.open("test.png")
+    processed_image = image.resize((500, 500))
+    processed_image.save("test.png")
+    path_img_1 = "test.png"
+
+if (os.path.exists("test2.png")):
+    image2 = Image.open("test2.png")
+
+    processed_image2 = image2.resize((500, 500))
+    processed_image2.save("test2.png")
+    path_img_2 = "test2.png"
+
+if (path_img_1 != "" and path_img_1 != None):
+    list_path.append(path_img_1)
+
+if (path_img_2 != "" and path_img_2 != None):
+    list_path.append(path_img_2)
+
+button = Button(root,text = "Display image", command= lambda : displayImages(root, path_img_1) )
+button1 = Button(root, text= "Mars Image", command = lambda : displayImages(root, path_img_2))
+button2 = Button(root,text = "Exit", command= lambda : close_window(root, list_path) )
+
 button.pack()
+button1.pack()
 button2.pack()
 
 root.mainloop()
